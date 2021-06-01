967992a7fb1175a5ead7432f2d4fac7390260a33
aika-l <67886657+aika-l@users.noreply.github.com>
2021-06-01 02:58:51 -0500

    Initial commit

---
+++ src/data.jsx
@@ -0,0 +1,37 @@
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text:
+      "I'm baby carrot, who was grown in the yard. How do you like this text of mine hahaha. "
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text: "Some text will be here. Un vino veritas. "
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text: "Here comes yet another text. "
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text: "will find out some text here. "
+  }
+];
+
+export default reviews;
